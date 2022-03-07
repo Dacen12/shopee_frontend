ef3962066bbeabca6e58cd2d24ffbd2a9baacee6
Dacen12 <alkisalki1@hotmail.com>
2022-03-07 13:29:18 +0100

    updated api routes

--- src/misc/useData.js
+++ src/misc/useData.js
@@ -6,7 +6,7 @@ function useData(path = '') {
     const [stockData, setStockData] = useState()
     const [modifiedArray, setModifiedArray] = useState([])
     function getData () {
-        axios.get(`${local_route}${path}`).then((res) => {
+        axios.get(`${public_route}${path}`).then((res) => {
             setStockData(res.data)
             setModifiedArray(res.data)
     
